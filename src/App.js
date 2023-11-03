d82f86e81097b3d52d4a1b4f4ead177e5addd8d3
nijaskhan <nijaskhan93@gmail.com>
2023-11-03 11:24:23 +0530

    safeAreaView added with style flex:1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { appStyles } from './styles/appStyles';
 
 const App = () => {
     return (
-        <>
+        <SafeAreaView style={appStyles.container}>
             <Header />
             <Body />
-        </>
+        </SafeAreaView>
     );
 }
 
