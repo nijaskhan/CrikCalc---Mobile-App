58eba346c54a61c251ee354e5efbae2b84a43dc8
nijaskhan <nijaskhan93@gmail.com>
2024-01-19 12:49:37 +0530

    url changed

--- src/constants/serverConst.js
+++ src/constants/serverConst.js
@@ -1,2 +1,3 @@
 // export const BASEURL = 'http://10.0.2.2:5000';
-export const BASEURL = 'https://backend-apis-r9fx.onrender.com';
+// export const BASEURL = 'https://backend-apis-r9fx.onrender.com';
+export const BASEURL = 'https://crickapp.parel.co';
