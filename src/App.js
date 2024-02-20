4514e37c4a8dfe3aab7864faded10a28a4b2a68c
Nijas <nijas@parelstudio.com>
2024-02-20 11:30:05 +0530

    code push integrated

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import Toast from 'react-native-toast-message';
 import CreateAppContext from './store/AppContext';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomePage from './pages/HomePage';
 import SummaryPage from './pages/SummaryPage';
 import SelectTeams from './pages/SelectTeams';
 import TossSelectPage from './pages/TossSelectPage';
 import LandingPage from './pages/LandingPage';
 import HistorySummary from './pages/HistorySummary';
+import CodePush from 'react-native-code-push';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-    return (
-        <>
-            <CreateAppContext>
-                <NavigationContainer>
-                    <Stack.Navigator screenOptions={{ headerShown: false }}>
-                        <Stack.Screen name='landingPage' component={LandingPage} />
-                        <Stack.Screen name='HistorySummaryPage' component={HistorySummary} />
-                        <Stack.Screen name='SelectOverPage' component={SelectTeams} />
-                        <Stack.Screen name='SelectBattingTeam' component={TossSelectPage} />
-                        <Stack.Screen name='HomePage' component={HomePage} />
-                        <Stack.Screen name='SummaryPage' component={SummaryPage} />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </CreateAppContext>
-            <Toast />
-        </>
-    );
-}
+  return (
+    <>
+      <CreateAppContext>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="landingPage" component={LandingPage} />
+            <Stack.Screen
+              name="HistorySummaryPage"
+              component={HistorySummary}
+            />
+            <Stack.Screen name="SelectOverPage" component={SelectTeams} />
+            <Stack.Screen name="SelectBattingTeam" component={TossSelectPage} />
+            <Stack.Screen name="HomePage" component={HomePage} />
+            <Stack.Screen name="SummaryPage" component={SummaryPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </CreateAppContext>
+      <Toast />
+    </>
+  );
+};
 
-export default App;
+export default CodePush(App);
